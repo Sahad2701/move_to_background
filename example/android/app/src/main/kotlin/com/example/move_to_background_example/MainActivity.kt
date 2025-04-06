81a25a5704a43e6e1d25b02969108ab8d470b297
Sahad2701 <skachhawala33@gmail.com>
2025-04-06 16:58:30 +0530

    upgrade: move_to_background plugin for Flutter 3.29 & native

---
+++ example/android/app/src/main/kotlin/com/example/move_to_background_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.move_to_background_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- ios/Classes/MoveToBackgroundPlugin.m
+++ ios/Classes/MoveToBackgroundPlugin.m
@@ -1,10 +1,11 @@
 #import "MoveToBackgroundPlugin.h"
 
 @implementation MoveToBackgroundPlugin
+
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"move_to_background"
-            binaryMessenger:[registrar messenger]];
+                                   methodChannelWithName:@"move_to_background"
+                                   binaryMessenger:[registrar messenger]];
   MoveToBackgroundPlugin* instance = [[MoveToBackgroundPlugin alloc] init];
   [registrar addMethodCallDelegate:instance channel:channel];
 }
@@ -12,7 +13,19 @@
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"moveTaskToBack" isEqualToString:call.method]) {
     UIApplication *app = [UIApplication sharedApplication];
-    [app performSelector:@selector(suspend)];
+
+    if ([app respondsToSelector:@selector(suspend)]) {
+      #pragma clang diagnostic push
+      #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+      [app performSelector:@selector(suspend)];
+      #pragma clang diagnostic pop
+
+      result(@(YES));
+    } else {
+      result([FlutterError errorWithCode:@"UNAVAILABLE"
+                                 message:@"Cannot move app to background."
+                                 details:nil]);
+    }
   } else {
     result(FlutterMethodNotImplemented);
   }
